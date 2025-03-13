738dc75b0c9624be3f737b23a59d8474aef899b0
jparep <parepson@gmail.com>
2025-03-12 23:37:33 -0600

    Updated models

--- flake_dbt/models/example/my_first_dbt_model.sql
+++ flake_dbt/models/example/my_first_dbt_model.sql
@@ -1,27 +1,14 @@
-
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
+-- Optimize: Removed unnecessary comments and improved formatting
 
 {{ config(materialized='table') }}
 
 with source_data as (
-
     select 1 as id
     union all
     select null as id
-
 )
 
 select *
 from source_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
+-- Uncomment the line below to exclude records with null `id` values
 -- where id is not null
